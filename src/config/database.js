4dec2f9dc9861ea6f2fa8e913cfd558187e2f277
yJeanx11x <j3anlucas010@gmail.com>
2026-03-18 16:46:05 -0300

    fix: Corrija a instância do Sequelize para autenticação no banco de dados

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,14 @@
 require('dotenv').config()
-const Sequelize = require("sequelize")
+const sequelize = require("sequelize")
 
-const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+const Sequelize = new sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
     dialect: 'mysql',
 
 })
 
 try {
-    sequelize.authenticate();
+    Sequelize.authenticate();
     console.log('A conexão foi estabelecida com sucesso.');
 } catch (error) {
     console.error('Não foi possível conectar ao banco de dados:', error);

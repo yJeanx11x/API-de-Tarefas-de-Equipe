85b6049bca45b0d138ba69b2da0ef649ed457727
yJeanx11x <j3anlucas010@gmail.com>
2026-03-18 16:30:28 -0300

    feat: Adicione arquivos de configuração do servidor e banco de dados

---
+++ src/app/server.js
@@ -0,0 +1,6 @@
+require('dotenv').config()
+const express = require("express")
+const app = express()
+app.use(express.json())
+
+app.listen(process.env.PORT, () => console.log('Servidor rodando com sucesso!'))
---
+++ src/config/database.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+const Sequelize = require("sequelize")
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+
+})
+
+try {
+    sequelize.authenticate();
+    console.log('A conexão foi estabelecida com sucesso.');
+} catch (error) {
+    console.error('Não foi possível conectar ao banco de dados:', error);
+}
+
+module.exports = {
+    Sequelize: Sequelize,
+    sequelize: sequelize
+}

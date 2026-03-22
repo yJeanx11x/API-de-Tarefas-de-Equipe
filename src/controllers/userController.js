d6d76e76ea4b761d6cd6f70079d788002e7b9763
yJeanx11x <j3anlucas010@gmail.com>
2026-03-22 15:02:46 -0300

    feat: adicione funcionalidade de login com JWT e ajuste nas rotas

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,13 +1,15 @@
+require('dotenv').config()
 const User = require('../models/User')
 const validacao = require('../middlewares/validationMiddleware')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 
 // criar User
 async function CriarUser(req, res, next) {
 
     const z = validacao.safeParse(req.body)
-  
+
     try {
         const passwordHash = await bcrypt.hash(z.data.password, 12)
 
@@ -24,4 +26,36 @@ async function CriarUser(req, res, next) {
     }
 
 }
-module.exports = { CriarUser }
+// login com jwt 
+
+async function login(req, res, next) {
+    const { email, password } = req.body
+    
+    try {
+        const contaExitente = await User.User.findOne({ where: { email } })
+        
+        if  (!contaExitente) {
+            return res.status(404).json({ message: 'E-mail inválido' })
+        }
+        
+        const Senhausuario = await bcrypt.compare(password, contaExitente.password)
+        if (!Senhausuario) {
+            return res.status(404).json({ message: 'Senha inválido' })
+
+        }
+
+        const secret = process.env.SECRET
+        const token = jwt.sign({
+            id: contaExitente._id
+        }, secret)
+
+        return res.status(200).json({ message: 'Login com sucesso', token })
+
+
+    } catch (error) {
+        next(error)
+    }
+
+}
+
+module.exports = { CriarUser, login }
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,5 +4,6 @@ const appRoutes = express.Router()
 const controllers = require('../controllers/userController')
 
 appRoutes.post('/auth/register', controllers.CriarUser)
+appRoutes.post('/auth/login', controllers.login)
 
 module.exports = appRoutes

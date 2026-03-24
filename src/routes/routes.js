15fb7294ce318a51a3b52cab208d3dce865ad4f7
yJeanx11x <j3anlucas010@gmail.com>
2026-03-24 16:32:16 -0300

    feat: adicione validação JWT nas rotas de criação de tarefa

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,9 +1,11 @@
 const express = require("express")
 const appRoutes = express.Router()
+const jwt = require('../config/jtw')
 
 const controllers = require('../controllers/userController')
 
 appRoutes.post('/auth/register', controllers.CriarUser)
 appRoutes.post('/auth/login', controllers.login)
+appRoutes.post('/tasks/:id', jwt.validacaJWT, controllers.CriarTarefa)
 
 module.exports = appRoutes
